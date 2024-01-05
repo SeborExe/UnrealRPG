cb74c6726cbcd2e95f676a413cb6589dcb1bc34b
Sebastian Hara <haradevelopment@gmail.com>
2024-01-05 12:50:31 +0100

    Basic functions
    Player input system. Highligt enemies when under cursor.

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,105 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (CursorHit.bBlockingHit)
+	{
+		LastActor = ThisActor;
+		ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+
+		if (LastActor == nullptr)
+		{
+			if (ThisActor != nullptr)
+			{
+				ThisActor->HighlightActor();
+			}
+			else
+			{
+				//both are null - do nothing
+			}
+		}
+		else
+		{
+			if (ThisActor == nullptr)
+			{
+				LastActor->UnHighlightActor();
+			}
+			else
+			{
+				if (LastActor != ThisActor)
+				{
+					LastActor->UnHighlightActor();
+					ThisActor->HighlightActor();
+				}
+				else
+				{
+					//Last actor is equal this actor - do nothing
+				}
+			}
+		}
+	}
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InpitAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InpitAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InpitAxisVector.X);
+	}
+}
